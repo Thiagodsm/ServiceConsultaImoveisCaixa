dcfe91afbbc9201bb30aa4b58d3eb46292fafbf1
Thiago Moreira <48686609+Thiagodsm@users.noreply.github.com>
2024-01-31 19:38:29 -0300

    Ajustando o codigo para deixar mais modular

--- ConsultaImoveisLeilaoCaixa/TaskConsultaImoveisCaixa.cs
+++ ConsultaImoveisLeilaoCaixa/TaskConsultaImoveisCaixa.cs
@@ -34,120 +34,14 @@ namespace ConsultaImoveisLeilaoCaixa
 
                 try
                 {
-                    #region Navegacao
-                    // Captura o título da nova página
-                    var newPageTitle = driver.Title;
-                    Console.WriteLine($"Novo Título da Página: {newPageTitle}");
-
-                    // Navegue para a página da Caixa
-                    driver.Navigate().GoToUrl("https://venda-imoveis.caixa.gov.br/sistema/busca-imovel.asp?sltTipoBusca=imoveis");
-
-                    // Selecione o estado (SP) no dropdown
-                    var estadoDropdown = new SelectElement(driver.FindElement(By.Id("cmb_estado")));
-                    estadoDropdown.SelectByText("SP");
-
-                    // Aguarde a página carregar as cidades
-                    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                    wait.Until(d => d.FindElement(By.Id("cmb_cidade")));
-
-                    // Aguarde um tempo adicional (de 5 segundos) antes de selecionar a cidade
-                    Thread.Sleep(5000);
-
-                    // Selecione a cidade (Guarujá) no dropdown
-                    var cidadeDropdown = new SelectElement(driver.FindElement(By.Id("cmb_cidade")));
-                    cidadeDropdown.SelectByText("GUARUJA");
-
-                    // Aguarde um tempo adicional (de 5 segundos) antes de clicar em proximo
-                    Thread.Sleep(5000);
-
-                    // Clique no botão "Próximo"
-                    var btnNext = driver.FindElement(By.Id("btn_next0"));
-                    btnNext.Click();
-
-                    // Aguarde um tempo adicional (de 5 segundos) antes de clicar em proximo
-                    Thread.Sleep(5000);
-
-                    // Clique no botão "Próximo"
-                    var btnNext1 = driver.FindElement(By.Id("btn_next1"));
-                    btnNext1.Click();
-
-                    // Aguarde um tempo adicional (de 20 segundos) antes de verificar a quantidade de paginas
-                    Thread.Sleep(20000);
+                    // Navega nas paginas do site da Caixa
+                    int totalPages = Navegacao(driver);
 
                     // Conjunto para rastrear números de imóveis já processados
-                    List<string> numerosImoveisProcessados = new List<string>();
+                    List<string> numerosImoveisProcessados = BuscaIdsImoveis(driver, totalPages);
 
-                    // Obtém o número total de páginas
-                    var totalPages = int.Parse(driver.FindElement(By.Id("hdnQtdPag")).GetAttribute("value"));
-                    #endregion Navegacao
-
-                    #region Buscando Id's dos imoveis
-                    for (int currentPage = 1; currentPage <= totalPages; currentPage++)
-                    {
-                        // Lógica para extrair detalhes de cada imóvel na página atual
-                        ReadOnlyCollection<IWebElement> detalhesLinks = driver.FindElements(By.CssSelector("a[onclick*='detalhe_imovel']"));
-
-                        foreach (IWebElement detalhesLink in detalhesLinks)
-                        {
-                            string onclickValue = detalhesLink.GetAttribute("onclick");
-                            string numeroImovel = ExtrairNumeroImovel(onclickValue);
-                            //detalhesLink.Click();
-
-                            // Adiciona o número do imóvel a lista
-                            numerosImoveisProcessados.Add(numeroImovel);
-                        }
-
-                        // Aguarde um tempo para a próxima página carregar completamente
-                        Thread.Sleep(10000);
-
-                        // Navegue para a próxima página, se houver
-                        if (currentPage < totalPages)
-                        {
-                            // Clique no link para a próxima página
-                            driver.FindElement(By.CssSelector($"a[href='javascript:carregaListaImoveis({currentPage + 1});']")).Click();
-
-                            // Aguarde um tempo para a próxima página carregar completamente
-                            Thread.Sleep(10000);
-                        }
-                    }
-                    // Removendo Id's duplicados
-                    numerosImoveisProcessados = numerosImoveisProcessados.Distinct().ToList();
-                    #endregion Buscando Id's dos imoveis
-
-                    #region Extraindo dados dos imoveis
-                    List<DadosImovel> dadosImoveis = new List<DadosImovel>();
-
-                    // Itera sobre os números de imóveis processados
-                    foreach (var numeroImovel in numerosImoveisProcessados)
-                    {
-                        // Executa o script JavaScript para chamar o método detalhe_imovel com o ID correspondente
-                        string script = $"detalhe_imovel({numeroImovel});";
-                        ((IJavaScriptExecutor)driver).ExecuteScript(script);
-
-                        // Aguarde um tempo para a próxima página carregar completamente (opcional)
-                        Thread.Sleep(1000);
-
-                        // Obtenha os dados esperados
-                        // Localiza a div principal que contém as informações
-                        IWebElement divPrincipal = driver.FindElement(By.CssSelector("div.content-wrapper.clearfix"));
-                        // Localiza a div pelo ID "galeria-imagens"
-                        IWebElement divGaleriaImagens = driver.FindElement(By.Id("galeria-imagens"));
-
-                        // Definindo o objeto a partir da div principal
-                        DadosImovel imovel = DefineObjeto(driver, divPrincipal, divGaleriaImagens);
-                        dadosImoveis.Add(imovel);
-
-                        // Após lidar com a página de detalhes, você pode voltar à lista de imóveis
-                        driver.Navigate().Back();
-                    }
-
-                    // Aguarde um tempo para voltar a pagina anterior
-                    Thread.Sleep(5000);
-
-                    // Volte para a página de listagem de imóveis
-                    driver.Navigate().Back();
-                    driver.Quit();
-                    #endregion Extraindo dados dos imoveis
+                    // Extrai as informações do site da caixa em forma de objeto
+                    List<DadosImovel> dadosImoveis = ExtraiDadosImoveisCaixa(driver, numerosImoveisProcessados);
 
                     // Salvando informacoes dos imoveis
                     ImoveisLeilaoCaixa imoveisLeilaoCaixa = new ImoveisLeilaoCaixa();
@@ -159,9 +53,11 @@ namespace ConsultaImoveisLeilaoCaixa
                     string imoveisCarregadosString = LerArquivoJson(Config.CaminhoArquivoImoveis);
                     ImoveisLeilaoCaixa imoveisCarregados = ConverterJsonParaObjeto(imoveisCarregadosString);
 
+                    //temporario
+                    //imoveisLeilaoCaixa = imoveisCarregados;
+
                     if (imoveisLeilaoCaixa.imoveis.Count > 0)
                     {
-                        //dadosImoveis = OrdenaListaImoveis(dadosImoveis);
                         bool succes = SalvarListaComoJson(imoveisLeilaoCaixa, Config.CaminhoArquivoImoveis);
 
                         string caminhoArquivo = Config.CaminhoArquivoImoveis;
@@ -172,10 +68,10 @@ namespace ConsultaImoveisLeilaoCaixa
                         foreach (DadosImovel item in imoveisLeilaoCaixa.imoveis)
                         {
                             mensagem = MontaMensagamTelegram(item);
-                            //bool retTelegram = await EnviarMensagemTelegram(tokenTelegram, chatIdTelegram, mensagem);
                             bool retTelegram = await EnviarMensagemComFoto(tokenTelegram, chatIdTelegram, mensagem, item.dadosVendaImovel.LinkImagensImovel.FirstOrDefault());
                         }
                     }
+                    Thread.Sleep(36000000);
                 }
                 catch (Exception e)
                 {
@@ -183,7 +79,7 @@ namespace ConsultaImoveisLeilaoCaixa
                     if (!String.IsNullOrWhiteSpace(e.Message))
                     {
                         driver.Quit();
-                        Thread.Sleep(10000);
+                        Thread.Sleep(60000);
                         await ExecuteAsync(stoppingToken);
                     }
                 }
@@ -194,6 +90,159 @@ namespace ConsultaImoveisLeilaoCaixa
                 }
             }
         }
+
+        #region Navegacao
+        public int Navegacao(EdgeDriver driver)
+        {
+            try
+            {
+                // Captura o título da nova página
+                var newPageTitle = driver.Title;
+                Console.WriteLine($"Novo Título da Página: {newPageTitle}");
+
+                // Navegue para a página da Caixa
+                driver.Navigate().GoToUrl("https://venda-imoveis.caixa.gov.br/sistema/busca-imovel.asp?sltTipoBusca=imoveis");
+
+                // Selecione o estado (SP) no dropdown
+                var estadoDropdown = new SelectElement(driver.FindElement(By.Id("cmb_estado")));
+                estadoDropdown.SelectByText("SP");
+
+                // Aguarde a página carregar as cidades
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.FindElement(By.Id("cmb_cidade")));
+
+                // Aguarde um tempo adicional (de 5 segundos) antes de selecionar a cidade
+                Thread.Sleep(5000);
+
+                // Selecione a cidade (Guarujá) no dropdown
+                var cidadeDropdown = new SelectElement(driver.FindElement(By.Id("cmb_cidade")));
+                cidadeDropdown.SelectByText("GUARUJA");
+
+                // Aguarde um tempo adicional (de 5 segundos) antes de clicar em proximo
+                Thread.Sleep(5000);
+
+                // Clique no botão "Próximo"
+                var btnNext = driver.FindElement(By.Id("btn_next0"));
+                btnNext.Click();
+
+                // Aguarde um tempo adicional (de 5 segundos) antes de clicar em proximo
+                Thread.Sleep(5000);
+
+                // Clique no botão "Próximo"
+                var btnNext1 = driver.FindElement(By.Id("btn_next1"));
+                btnNext1.Click();
+
+                // Aguarde um tempo adicional (de 20 segundos) antes de verificar a quantidade de paginas
+                Thread.Sleep(20000);
+
+                // Obtém o número total de páginas
+                int totalPages = int.Parse(driver.FindElement(By.Id("hdnQtdPag")).GetAttribute("value"));
+                return totalPages;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+        #endregion Navegacao
+
+        #region BuscaIdsImoveis
+        public List<string> BuscaIdsImoveis(EdgeDriver driver,  int totalPages)
+        {
+            try
+            {
+                // Conjunto para rastrear números de imóveis já processados
+                List<string> numerosImoveisProcessados = new List<string>();
+
+
+                for (int currentPage = 1; currentPage <= totalPages; currentPage++)
+                {
+                    // Lógica para extrair detalhes de cada imóvel na página atual
+                    ReadOnlyCollection<IWebElement> detalhesLinks = driver.FindElements(By.CssSelector("a[onclick*='detalhe_imovel']"));
+
+                    foreach (IWebElement detalhesLink in detalhesLinks)
+                    {
+                        string onclickValue = detalhesLink.GetAttribute("onclick");
+                        string numeroImovel = ExtrairNumeroImovel(onclickValue);
+                        //detalhesLink.Click();
+
+                        // Adiciona o número do imóvel a lista
+                        numerosImoveisProcessados.Add(numeroImovel);
+                    }
+
+                    // Aguarde um tempo para a próxima página carregar completamente
+                    Thread.Sleep(10000);
+
+                    // Navegue para a próxima página, se houver
+                    if (currentPage < totalPages)
+                    {
+                        // Clique no link para a próxima página
+                        driver.FindElement(By.CssSelector($"a[href='javascript:carregaListaImoveis({currentPage + 1});']")).Click();
+
+                        // Aguarde um tempo para a próxima página carregar completamente
+                        Thread.Sleep(10000);
+                    }
+                }
+                // Removendo Id's duplicados
+                numerosImoveisProcessados = numerosImoveisProcessados.Distinct().ToList();
+                return numerosImoveisProcessados;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+            
+        }
+        #endregion BuscaIdsImoveis
+
+        #region ExtraiDadosImoveisCaixa
+        public List<DadosImovel> ExtraiDadosImoveisCaixa(EdgeDriver driver, List<string> numerosImoveisProcessados)
+        {
+            try
+            {
+                List<DadosImovel> dadosImoveis = new List<DadosImovel>();
+
+                // Itera sobre os números de imóveis processados
+                foreach (var numeroImovel in numerosImoveisProcessados)
+                {
+                    // Executa o script JavaScript para chamar o método detalhe_imovel com o ID correspondente
+                    string script = $"detalhe_imovel({numeroImovel});";
+                    ((IJavaScriptExecutor)driver).ExecuteScript(script);
+
+                    // Aguarde um tempo para a próxima página carregar completamente (opcional)
+                    Thread.Sleep(1000);
+
+                    // Obtenha os dados esperados
+                    // Localiza a div principal que contém as informações
+                    IWebElement divPrincipal = driver.FindElement(By.CssSelector("div.content-wrapper.clearfix"));
+                    // Localiza a div pelo ID "galeria-imagens"
+                    IWebElement divGaleriaImagens = driver.FindElement(By.Id("galeria-imagens"));
+
+                    // Definindo o objeto a partir da div principal
+                    DadosImovel imovel = DefineObjeto(driver, divPrincipal, divGaleriaImagens);
+                    dadosImoveis.Add(imovel);
+
+                    // Após lidar com a página de detalhes, você pode voltar à lista de imóveis
+                    driver.Navigate().Back();
+                }
+
+                // Aguarde um tempo para voltar a pagina anterior
+                Thread.Sleep(5000);
+
+                // Volte para a página de listagem de imóveis
+                driver.Navigate().Back();
+                driver.Quit();
+                return dadosImoveis;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+        #endregion ExtraiDadosImoveisCaixa
 
         #region ExtrairNumeroImovel
         public string ExtrairNumeroImovel(string onclickValue)
@@ -309,7 +358,7 @@ namespace ConsultaImoveisLeilaoCaixa
                 return textoTratado;
             }
             else
-                return String.Empty;
+                return "-";
         }
         #endregion ExtraiDadosImovel
 
@@ -327,7 +376,7 @@ namespace ConsultaImoveisLeilaoCaixa
                 return textoTratado;
             }
             else
-                return String.Empty;
+                return "-";
         }
         #endregion ExtraiDadosVendaImovel
 
@@ -390,7 +439,7 @@ namespace ConsultaImoveisLeilaoCaixa
                     pattern = @"\(\s*desconto de (\d+(,\d+)?)%\)";
                     break;
                 default:
-                    return "";
+                    return "-";
             }
 
             Match match = Regex.Match(text, pattern);
@@ -400,7 +449,7 @@ namespace ConsultaImoveisLeilaoCaixa
                 return parameter == "desconto" ? match.Groups[1].Value : match.Groups[1].Value;
             }
 
-            return "";
+            return "-";
         }
         #endregion ExtraiValor
 
